ba99f4cb2ef237eaad09ed0d59488ebb94dbda8d
ABCLabDB <145737797+ABCLabDB@users.noreply.github.com>
2026-02-22 19:08:46 +0900

    Update 00_setup.R

--- scripts/00_setup.R
+++ scripts/00_setup.R
@@ -1,4 +1,11 @@
-## scripts/00_setup.R
+############################################################
+# 00_setup.R
+#
+# Install and load R packages required for Sleep Evolution
+# analyses. Run once from the repository root.
+#
+# Required working directory: Sleep_Evolution/
+############################################################
 
 required_packages <- c(
   "data.table",
@@ -16,8 +23,10 @@ for (pkg in required_packages) {
   }
 }
 
-library(data.table)
-library(dplyr)
-library(ggplot2)
-library(ggpubr)
-library(DescTools)
+suppressPackageStartupMessages({
+  library(data.table)
+  library(dplyr)
+  library(ggplot2)
+  library(ggpubr)
+  library(DescTools)
+})
