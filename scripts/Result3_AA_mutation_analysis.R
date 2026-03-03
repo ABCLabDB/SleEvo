1a4fbca29ce2aa8a0ace564ec5e833423c626e42
ABCLabDB <145737797+ABCLabDB@users.noreply.github.com>
2026-03-03 22:33:27 +0900

    Update Result3_AA_mutation_analysis.R

--- scripts/Result3_AA_mutation_analysis.R
+++ scripts/Result3_AA_mutation_analysis.R
@@ -1,10 +1,31 @@
 ############################################################
-# Script: Amino Acid Mutation Analysis for NREM-Associated SNPs
+# Script: Amino Acid Mutation Analysis and Visualization
 # Description:
-# This script identifies non-synonymous mutations associated
-# with NREM ratio variation and annotates amino acid
-# physicochemical property changes.
-# Output: Supplementary_table11.tsv
+#   This script performs the following analyses:
+#
+#   1) Identifies non-synonymous mutations associated with
+#      NREM ratio variation from significant SNP sites.
+#   2) Compares original and mutated codons relative to the
+#      human reference CDS.
+#   3) Annotates amino acid physicochemical property changes.
+#   4) Exports mutation table (Supplementary_table11.tsv).
+#   5) Generates protein domain mutation visualization
+#      for selected genes (Figure4F).
+#
+# Input:
+#   - Species sleep metadata
+#   - NREM ratio enrichment results
+#   - Significant SNP table
+#   - CDS nucleotide alignment matrices
+#
+# Output:
+#   - Supplementary_table11.tsv
+#   - Protein mutation domain plot (Figure4F.pdf)
+#
+# Notes:
+#   - Only non-synonymous mutations are retained.
+#   - Human CDS is used as the reference sequence.
+#   - Physicochemical property changes are annotated.
 ############################################################
 
 
@@ -307,5 +328,161 @@ fwrite(mutation_results,
        quote = FALSE)
 
 ############################################################
-# End of Script
+# 10. Protein Domain Mutation Plot (Figure4F)
+############################################################
+
+library(ggrepel)
+library(ggplot2)
+library(dplyr)
+
+# Load generated mutation table
+mutation_results_plot <- as.data.frame(
+  fread(OUTPUT_FILE)
+)
+
+# Remove row index column if present
+mutation_results_plot <- mutation_results_plot[,-c(1)]
+
+# Clean Position format
+mutation_results_plot$Position <-
+  sapply(strsplit(mutation_results_plot$Position,"_"),
+         function(x) x[2])
+
+mutation_results_plot$Position <-
+  paste0("Chr",
+         mutation_results_plot$Chr,
+         ":",
+         mutation_results_plot$Position)
+
+############################################################
+# Filter Target Gene (ARNTL2)
+############################################################
+
+protein_Gene <- mutation_results_plot %>%
+  filter(Gene == "ARNTL2") %>%
+  mutate(
+    mutation_label =
+      paste0(Original_Amino_Acid,
+             Codon_Position,
+             Mutated_Amino_Acid,
+             " (",Position,")"),
+    
+    y = ifelse(Category == "long", 1.1, 1),
+    
+    yend = ifelse(Category == "long",
+                  runif(n(), min = 1.3, max = 1.38),
+                  runif(n(), min = 0.65, max = 0.75)),
+    
+    label_y = yend
+  )
+
+full_length <- unique(protein_Gene$Full_Length)
+
+############################################################
+# Force specific mutation label to be visible
+############################################################
+
+protein_Gene$mutation_label[
+  protein_Gene$Position != "Chr12:27401345"
+] <- protein_Gene$mutation_label[
+  protein_Gene$Position != "Chr12:27401345"
+]
+
+############################################################
+# Domain annotation
+############################################################
+
+domain_df <- data.frame(
+  name  = c("bHLH","PAS","PAS","PAC"),
+  start = c(107,178,357,432),
+  end   = c(160,250,427,475),
+  fill  = c("#fbf2c4","#74a892","#74a892","#003f5c")
+)
+
+############################################################
+# Plot
+############################################################
+
+p1 <- ggplot() +
+  
+  geom_rect(aes(xmin = 1,
+                xmax = full_length,
+                ymin = 1,
+                ymax = 1.1),
+            fill = "#ffffff",
+            color = "black") +
+  
+  geom_rect(data = domain_df,
+            aes(xmin = start,
+                xmax = end,
+                ymin = 1,
+                ymax = 1.1,
+                fill = name),
+            color = "black",
+            alpha = 0.8) +
+  
+  geom_segment(data = protein_Gene,
+               aes(x = Codon_Position,
+                   xend = Codon_Position,
+                   y = y,
+                   yend = yend),
+               color = "black") +
+  
+  geom_point(data = protein_Gene,
+             aes(x = Codon_Position,
+                 y = yend,
+                 color = Category),
+             size = 3) +
+  
+  geom_text_repel(
+    data = protein_Gene,
+    aes(x = Codon_Position,
+        y = yend,
+        label = mutation_label),
+    direction = "y",
+    size = 3.5,
+    fontface = "bold",
+    box.padding = 0.2,
+    point.padding = 0.2,
+    segment.color = NA,
+    max.overlaps = Inf
+  ) +
+  
+  scale_fill_manual(values = setNames(domain_df$fill,
+                                      domain_df$name)) +
+  
+  scale_x_continuous(
+    breaks = seq(0, full_length, by = 100),
+    limits = c(0, full_length + 10)
+  ) +
+  
+  coord_cartesian(ylim = c(0.6, 1.5)) +
+  
+  theme_minimal() +
+  theme(
+    axis.text.y = element_blank(),
+    axis.title.y = element_blank(),
+    axis.ticks.y = element_blank(),
+    panel.grid = element_blank(),
+    axis.title.x = element_text(face = "bold"),
+    plot.title = element_text(hjust = 0.5,
+                              face = "bold")
+  ) +
+  
+  labs(
+    x = "",
+    title = "ARNTL2 Gene Mutation"
+  )
+
+ggsave(
+  "/disk4/bijsy/2.Sleep/Figure/UPGMA_Bootstrapping/Evolution/Figure4F.pdf",
+  plot = p1,
+  width = 7,
+  height = 3,
+  units = "in",
+  dpi = 300
+)
+
+############################################################
+# End of Extended Script
 ############################################################
